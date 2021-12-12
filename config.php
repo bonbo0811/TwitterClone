5c3ecbf2dc7e1ed67a1d901fee5535114fbfd843
bonbo0811 <abcdef999gp811@gmail.com>
2021-12-12 23:27:48 +0900

    会員登録機能

--- config.php
+++ config.php
@@ -1,8 +1,12 @@
 <?php
 //エラー表示あり
-    ini_set('display errors',1) ;
+  ini_set('display errors',1) ;
  //日本時間に設定する
-    date_default_timezone_set('Asia/Tokyo');
+  date_default_timezone_set('Asia/Tokyo');
  //URL / ディレクトリ設定
-    define('HOME_URL','/twitterclone/');
-    
+  define('HOME_URL','/twitterclone/');
+// データベースの接続情報
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+define('DB_NAME','twitter_clone');
